5e7208475c85d72fb121e394fe4fcaf907baa4a7
turtleseason <48301106+turtleseason@users.noreply.github.com>
2021-02-06 18:24:00 -0800

    Break up EdgePathDrawer.getPathString()

--- puzzle.js
+++ puzzle.js
@@ -43,83 +43,61 @@
 
 	class EdgePathDrawer {
 		constructor(pieceWidth, pieceHeight, borderSize) {
-			this.pW = pieceWidth;
-			this.pH = pieceHeight;
-			this.b = borderSize;
+			this.START = 'START';
+			this.templates = this.makeTemplates(pieceWidth, pieceHeight, borderSize)
+		}
+
+		makeTemplates(pW, pH, b) {
+			const templates = {};
+
+			templates[this.START] = `M ${b} ${pH - b} `;
+
+			templates[BUMP + LEFT] = (w, o, c1, c2) => `V ${(pH + w) / 2}`	
+				+ `C ${b - o} ${pH / 2 + c1} ${0} ${pH / 2 + c2} ${0} ${pH / 2} `
+				+ `C ${0} ${pH / 2 - c2} ${b - o} ${pH / 2 - c1} ${b} ${(pH - w) / 2} `;
+			templates[RECESS + LEFT] = (w, o, c1, c2) => `V ${(pH + w) / 2}`
+				+ `C ${b + o} ${pH / 2 + c1} ${b * 2} ${pH / 2 + c2} ${b * 2} ${pH / 2} `
+				+ `C ${b * 2} ${pH / 2 - c2} ${b + o} ${pH / 2 - c1} ${b} ${(pH - w) / 2} `;
+			templates[LEFT] = `V ${b} `;
+
+			templates[BUMP + TOP] = (w, o, c1, c2) => `H ${(pW - w) / 2} `
+				+ `C ${pW / 2 - c1} ${b - o} ${pW / 2 - c2} ${0} ${pW / 2} ${0} `
+				+ `C ${pW / 2 + c2} ${0} ${pW / 2 + c1} ${b - o} ${(pW + w) / 2} ${b} `;
+			templates[RECESS + TOP] = (w, o, c1, c2) => `H ${(pW - w) / 2} `
+				+ `C ${pW / 2 - c1} ${b + o}  ${pW / 2 - c2} ${b * 2} ${pW / 2} ${b * 2} `
+				+ `C ${pW / 2 + c2} ${b * 2} ${pW / 2 + c1} ${b + o} ${(pW + w) / 2} ${b} `;
+			templates[TOP] = `H ${pW - b} `;
+
+			templates[BUMP + RIGHT] = (w, o, c1, c2) => `V ${(pH - w) / 2} `
+				+ `C ${pW - b + o} ${pH / 2 - c1} ${pW} ${pH / 2 - c2} ${pW} ${pH / 2} `
+				+ `C ${pW} ${pH / 2 + c2} ${pW - b + o} ${pH / 2 + c1} ${pW - b} ${(pH + w) / 2} `;
+			templates[RECESS + RIGHT] = (w, o, c1, c2) => `V ${(pH - w) / 2} `
+				+ `C ${pW - b - o} ${pH / 2 - c1} ${pW - b * 2} ${pH / 2 - c2} ${pW - b * 2} ${pH / 2} `
+				+ `C ${pW - b * 2} ${pH / 2 + c2} ${pW - b - o} ${pH / 2 + c1} ${pW - b} ${(pH + w) / 2} `;
+			templates[RIGHT] = `V ${pH - b} `;
+
+			templates[BUMP + BOTTOM] = (w, o, c1, c2) => `H ${(pW + w) / 2} `
+				+ `C ${pW / 2 + c1} ${pH - b + o} ${pW / 2 + c2} ${pH} ${pW / 2} ${pH} `
+				+ `C ${pW / 2 - c2} ${pH} ${pW / 2 - c1} ${pH - b + o} ${(pW - w) / 2} ${pH - b} `;
+			templates[RECESS + BOTTOM] = (w, o, c1, c2) => `H ${(pW + w) / 2} `
+				+ `C ${pW / 2 + c1} ${pH - b - o}  ${pW / 2 + c2} ${pH - b * 2} ${pW / 2} ${pH - b * 2} `
+				+ `C ${pW / 2 - c2} ${pH - b * 2} ${pW / 2 - c1} ${pH - b - o} ${(pW - w) / 2} ${pH - b} `;
+			templates[BOTTOM] = `Z`;
+
+			return templates;
 		}
 
 		getPathString(side, edgeStyleInfo) {
-			const [pW, pH, b] = [this.pW, this.pH, this.b];
-			let w, o, c1, c2;
-			if (edgeStyleInfo) {
-				[w, o, c1, c2] = [edgeStyleInfo.w, edgeStyleInfo.o, edgeStyleInfo.c1, edgeStyleInfo.c2];
+			const start = (side === LEFT) ? this.templates[this.START] : '';
+			const end = this.templates[side];
+
+			let mid = '';
+			if (edgeStyleInfo.type !== FLAT) {
+				const key = edgeStyleInfo.type + side;
+				mid = this.templates[key](edgeStyleInfo.w, edgeStyleInfo.o, edgeStyleInfo.c1, edgeStyleInfo.c2);
 			}
-			
-			const leftStart = `M ${b} ${pH - b} V ${(pH + w) / 2} `;
-			
-			const leftBump = `C ${b - o} ${pH / 2 + c1} ${0} ${pH / 2 + c2} ${0} ${pH / 2} `
-			+ `C ${0} ${pH / 2 - c2} ${b - o} ${pH / 2 - c1} ${b} ${(pH - w) / 2} `;
-			
-			const leftRecess = `C ${b + o} ${pH / 2 + c1} ${b * 2} ${pH / 2 + c2} ${b * 2} ${pH / 2} `
-			+ `C ${b * 2} ${pH / 2 - c2} ${b + o} ${pH / 2 - c1} ${b} ${(pH - w) / 2} `;
-			
-			const leftEnd = `V ${b} `;
-			
-			const topStart = `H ${(pW - w) / 2} `;
-			
-			const topBump = `C ${pW / 2 - c1} ${b - o} ${pW / 2 - c2} ${0} ${pW / 2} ${0} `
-						  + `C ${pW / 2 + c2} ${0} ${pW / 2 + c1} ${b - o} ${(pW + w) / 2} ${b} `;
-			
-			const topRecess = `C ${pW / 2 - c1} ${b + o}  ${pW / 2 - c2} ${b * 2} ${pW / 2} ${b * 2} `
-							+ `C ${pW / 2 + c2} ${b * 2} ${pW / 2 + c1} ${b + o} ${(pW + w) / 2} ${b} `;
-			
-			const topEnd = `H ${pW - b} `;
-			
-			const rightStart = `V ${(pH - w) / 2} `;
-			
-			const rightBump = `C ${pW - b + o} ${pH / 2 - c1} ${pW} ${pH / 2 - c2} ${pW} ${pH / 2} `
-			+ `C ${pW} ${pH / 2 + c2} ${pW - b + o} ${pH / 2 + c1} ${pW - b} ${(pH + w) / 2} `;
-			
-			const rightRecess = `C ${pW - b - o} ${pH / 2 - c1} ${pW - b * 2} ${pH / 2 - c2} ${pW - b * 2} ${pH / 2} `
-			+ `C ${pW - b * 2} ${pH / 2 + c2} ${pW - b - o} ${pH / 2 + c1} ${pW - b} ${(pH + w) / 2} `;
-			
-			const rightEnd = `V ${pH - b} `;
-			
-			const bottomStart = `H ${(pW + w) / 2} `;
-			
-			const bottomBump = `C ${pW / 2 + c1} ${pH - b + o} ${pW / 2 + c2} ${pH} ${pW / 2} ${pH} `
-						  	 + `C ${pW / 2 - c2} ${pH} ${pW / 2 - c1} ${pH - b + o} ${(pW - w) / 2} ${pH - b} `;
-			
-			const bottomRecess = `C ${pW / 2 + c1} ${pH - b - o}  ${pW / 2 + c2} ${pH - b * 2} ${pW / 2} ${pH - b * 2} `
-							   + `C ${pW / 2 - c2} ${pH - b * 2} ${pW / 2 - c1} ${pH - b - o} ${(pW - w) / 2} ${pH - b} `;
-			
-			const bottomEnd = `Z`;
-			
-			const type = edgeStyleInfo ? edgeStyleInfo.type : FLAT;
-			switch(side) {
-				case LEFT:
-					return leftStart
-					+ (type === BUMP ? leftBump : '')
-					+ (type === RECESS ? leftRecess : '') 
-					+ leftEnd;
-				case TOP:
-					return topStart
-					+ (type === BUMP ? topBump : '')
-					+ (type === RECESS ? topRecess : '') 
-					+ topEnd;
-				case RIGHT:
-					return rightStart
-					+ (type === BUMP ? rightBump : '')
-					+ (type === RECESS ? rightRecess : '')
-					+ rightEnd;
-				case BOTTOM:
-					return bottomStart
-					+ (type === BUMP ? bottomBump : '')
-					+ (type === RECESS ? bottomRecess : '') 
-					+ bottomEnd;
-				default:
-					return undefined;
-			}
+
+			return start + mid + end;
 		}
 	}
 
