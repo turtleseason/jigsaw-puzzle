2787592d63223049ad74ad884002ff2f7f679a55
turtleseason <48301106+turtleseason@users.noreply.github.com>
2021-02-16 19:45:38 -0800

    Import images in JS

--- src/providedImages.js
+++ src/providedImages.js
@@ -1,20 +1,26 @@
 import { ImageInfo, ImageSource } from './ImageInfo';
 
+import img1 from './images/ito-jakuchu-red-parrot-on-the-branch-of-a-tree.jpg';
+import img2 from './images/david-clode-eOSqRq2Qm1c-unsplash.jpg';
+import img3 from './images/casey-horner-80UR4DM2Rz0-unsplash.jpg';
+import img4 from './images/van-gogh-roses-nga.jpg';
+import img5 from './images/scott-webb-lYzgtps0UtQ-unsplash.jpg';
+import img6 from './images/travel-sourced-FsmcD6uKcHk-unsplash.jpg';
+
 
 const providedImages = [
-    new ImageInfo('Red Parrot on the Branch of a Tree', 'images/ito-jakuchu-red-parrot-on-the-branch-of-a-tree.jpg', 6, 8, 'Itō Jakuchū', 
+    new ImageInfo('Red Parrot on the Branch of a Tree', img1, 6, 8, 'Itō Jakuchū', 
         new ImageSource('Metropolitan Museum of Art', true, false, 'https://www.metmuseum.org/art/collection/search/57123'), 'Red Parrot'),
-    new ImageInfo('Coral', 'images/david-clode-eOSqRq2Qm1c-unsplash.jpg', 7, 10, 'David Clode',
+    new ImageInfo('Coral', img2, 7, 10, 'David Clode',
         new ImageSource('Unsplash', false, true, 'https://unsplash.com/photos/eOSqRq2Qm1c')),
-    new ImageInfo('Starry Sky', 'images/casey-horner-80UR4DM2Rz0-unsplash.jpg', 7, 10, 'Casey Horner',
+    new ImageInfo('Starry Sky', img3, 7, 10, 'Casey Horner',
         new ImageSource('Unsplash', false, true, 'https://unsplash.com/photos/80UR4DM2Rz0')),
-    new ImageInfo('Roses', 'images/van-gogh-roses-nga.jpg', 8, 10, 'Vincent van Gogh',
+    new ImageInfo('Roses', img4, 8, 10, 'Vincent van Gogh',
         new ImageSource('National Gallery of Art', true, false, 'https://www.nga.gov/collection/art-object-page.72328.html')),
-    new ImageInfo('Succulents', 'images/scott-webb-lYzgtps0UtQ-unsplash.jpg', 11, 7, 'Scott Webb',
+    new ImageInfo('Succulents', img5, 11, 7, 'Scott Webb',
         new ImageSource('Unsplash', false, true, 'https://unsplash.com/photos/lYzgtps0UtQ')),
-    new ImageInfo('Jellyfish', 'images/travel-sourced-FsmcD6uKcHk-unsplash.jpg', 7, 10, 'Travel Sourced',
-        new ImageSource('Unsplash', false, true, 'https://unsplash.com/photos/FsmcD6uKcHk')),
-    new ImageInfo('freebirb', 'lunar_festival.png', 7, 10, 'Kan Gao')
+    new ImageInfo('Jellyfish', img6, 7, 10, 'Travel Sourced',
+        new ImageSource('Unsplash', false, true, 'https://unsplash.com/photos/FsmcD6uKcHk'))
 ];
 
 export default providedImages;
