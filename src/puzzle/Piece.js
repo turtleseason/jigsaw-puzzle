2a640e9cbc072b9346df50434af60329731b12e1
turtleseason <48301106+turtleseason@users.noreply.github.com>
2021-03-04 01:24:40 -0800

    Fix for Firefox

--- src/puzzle/Piece.js
+++ src/puzzle/Piece.js
@@ -14,20 +14,25 @@ export default class Piece extends PureComponent {
         const model = this.props.model;
         const className = 'puzzle-piece' + (this.props.blockPointerEvents ? ' block-pointer-events' : '');
         return (
-            <svg
-                className={className}
-                data-id={model.key}
-                onPointerDown={(e) => this.props.onPointerDown(e)}
-                clipPath={'url(#clip-' + model.key + ')'}
-                style={{
-                    backgroundPosition: this.backgroundPositionString,
-                    width: this.props.width,
-                    height: this.props.height,
+            // Set the position on a wrapper div because if it's set directly on the svg, Firefox fails to re-render
+            // with the updated position when pieces 'snap' together, for some reason
+            <div style={{
                     left: model.displayPos.left,
                     top: model.displayPos.top,
-                    zIndex: (model.zIndex > 0 ? model.zIndex : 'auto')
-                }}>
-            </svg>
+                    position: 'absolute'}}>
+                <svg
+                    className={className}
+                    data-id={model.key}
+                    onPointerDown={(e) => this.props.onPointerDown(e)}
+                    clipPath={'url(#clip-' + model.key + ')'}
+                    style={{
+                        backgroundPosition: this.backgroundPositionString,
+                        width: this.props.width,
+                        height: this.props.height,
+                        zIndex: (model.zIndex > 0 ? model.zIndex : 'auto')
+                    }}>
+                </svg>
+            </div>
         );
     }
 }
